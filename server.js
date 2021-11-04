1a8a147a9a4721772a96d5474d4239456b99a4e0
caseyryan22465 <caseyryan22465@gmail.com>
2021-11-04 14:51:05 -0400

    Added Post endpoints

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//User Endpoints
+
 //create user
 app.post('/users/new', function (req, res) {
 
@@ -19,5 +21,28 @@ app.get('/users/:id', function (req, res) {
 
 //delete user. Uses POST for authentication (?)
 app.post('/users/:id/delete', function (req, res) {
+
+});
+
+//--------------------------------------------------------
+//Post Endpoints
+
+//create post
+app.post('/posts/new', function (req, res) {
+
+});
+
+//update post
+app.post('/posts/:id', function (req, res) {
+
+});
+
+//read post
+app.get('/posts/:id', function (req, res) {
+
+});
+
+//delete post. need POST for auth ?
+app.post('/posts/:id/delete', function (req, res) {
 
 });
