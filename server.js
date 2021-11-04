1069ae2c191cc25eb1f281ddd8b6d0517b0eb0a4
caseyryan22465 <caseyryan22465@gmail.com>
2021-11-04 14:47:40 -0400

    Server User endpoints

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
+//create user
+app.post('/users/new', function (req, res) {
+
+});
+
+//update user
+app.post('/users/:id', function (req, res) {
+
+})
+
+//read user
+app.get('/users/:id', function (req, res) {
+
+});
+
+//delete user. Uses POST for authentication (?)
+app.post('/users/:id/delete', function (req, res) {
+
+});
