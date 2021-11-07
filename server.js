c06b136b283a8b573bc4c1859c99c3dc99e494a8
caseyryan22465 <caseyryan22465@gmail.com>
2021-11-06 20:45:54 -0400

    fixed typo breaking server

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/posts/:id', function (req, res) {
     let subtags1 = [faker.lorem.word(), faker.lorem.word()];
     let subtags2 = [faker.lorem.word()];
     postdata['tags'] = {"l1tags":[tag1,tag2], tag1:subtags1, tag2:subtags2};
-    postdata['content'] = postData['contentType'] === 'image' ? {"imageUrl": faker.image.city()} : {"albumArt": faker.image.nightlife(), "songUrl": faker.internet.url()};
+    postdata['content'] = postdata['contentType'] === 'image' ? {"imageUrl": faker.image.city()} : {"albumArt": faker.image.nightlife(), "songUrl": faker.internet.url()};
     res.status(200);
     res.send(JSON.stringify(postdata));
 });
