136788a7ddffe9fd09903193d4e17692204ee331
caseyryan22465 <caseyryan22465@gmail.com>
2021-12-07 13:49:37 -0500

    signin frontend logic

---
+++ pages/signupPage/index.js
@@ -0,0 +1,25 @@
+document.getElementById('submitRegistration').addEventListener('click', async function() {
+    let postObj = {};
+    postObj['email'] = document.getElementById('floatingInput').value;
+    postObj['username'] = document.getElementById('floatingUsername').value;
+    postObj['password'] = document.getElementById('floatingPassword').value;
+    if(document.getElementById('floatingRepeat').value !== postObj['password']) {
+        alert('Passwords do not match');
+        return;
+    }
+
+    const res = await fetch("/api/users/new", {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postObj)
+      });
+    if(res.status === 201) {
+        window.location.href = '/login';
+    }
+    else{
+        alert("Error creating user");
+    }
+});
